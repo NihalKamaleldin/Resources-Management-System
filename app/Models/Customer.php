bf953c426c20af41ede37770984584e7a3d33ac1
NihalKamaleldin <nihal.elfadl@studio.unibo>
2023-07-09 22:23:41 +0200

    Created Customer views (create,edit and index), customer model and customer controller. created databese table using the migration feature, added the customer route to web.php route file, modified the js and css files.

---
+++ app/Models/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'photo',
+
+    ];
+
+    protected $guarded = [
+        'id',
+    ];
+
+
+
+}
